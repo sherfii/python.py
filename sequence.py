36981b81c9d0b0d06dce4406315c371340a6fcb9
sherfii <116794706+sherfii@users.noreply.github.com>
2023-11-03 11:46:47 +0530

    Add files via upload

--- sequence.py
+++ sequence.py
@@ -1,5 +1,6 @@
 n=int(input("enter the limit:"))
-for i in range (1,n+1)
+for i in range (1,n+1):
     for j in range(1,i+1):
-        print(i,end:"")
-        print("\r")1
+         print(i,end ="")
+         "");
+print("\r")
